382cb74c6962343c1e8bff7bb6e5f4bfdaf4900c
Slava Semushin <slava.semushin@gmail.com>
2020-07-05 14:11:33 +0100

    make a form to look like I want to see it :)
    [skip ci]

--- src/main/frontend/src/components/AddReleaseYearForm.js
+++ src/main/frontend/src/components/AddReleaseYearForm.js
@@ -104,48 +104,47 @@ class AddReleaseYearFormView extends React.PureComponent {
 		const rangeOfYears = this.generateRange(sinceYear, tillYear);
 		
 		return (
-			<div className="row">
-				<div id="add-release-year-failed-msg"
-					className={ `alert alert-danger text-center col-sm-8 col-sm-offset-2 ${hasServerError ? '' : 'hidden'}` }>
-					{ l10n['t_server_error'] || 'Server error' }
+			<React.Fragment>
+				<div className="row">
+					<div id="add-release-year-failed-msg"
+						className={ `alert alert-danger text-center col-sm-6 col-sm-offset-3 ${hasServerError ? '' : 'hidden'}` }>
+						{ l10n['t_server_error'] || 'Server error' }
+					</div>
 				</div>
-				<div className="col-sm-12">
-					<form id="add-release-year-form"
-						className={`form-horizontal ${hasValidationErrors ? 'has-error' : ''}`}
-						onSubmit={ handleSubmit }>
-						<label htmlFor="release-year" className="control-label col-sm-3">
-							{ l10n['t_year'] || 'Year' }
-						</label>
-						<div className="form-group form-group-sm col-sm-5">
-							<select
-								id="release-year"
-								name="release-year"
-								className="form-control"
-								required="required"
-								onChange={ handleChange }>
-								<option value=""></option>
-								{rangeOfYears.map(year => (
-									<option key={year.toString()} value={year}>{ year }</option>
-								))}
-							</select>
+				<div className="row">
+					<div className="col-sm-9 col-sm-offset-3 form-group">
+						<form id="add-release-year-form"
+							className={ `form-inline ${hasValidationErrors ? 'has-error' : ''}` }
+							onSubmit={ handleSubmit }>
+							<div className="form-group form-group-sm">
+								<select
+									id="release-year"
+									name="release-year"
+									className="form-control"
+									required="required"
+									onChange={ handleChange }>
+									<option value="">{ l10n['t_year'] || 'Year' }</option>
+									{rangeOfYears.map(year => (
+										<option key={year.toString()} value={year}>{ year }</option>
+									))}
+								</select>
+							</div>
+							<div className="form-group form-group-sm">
+								<button type="submit"
+									className="btn btn-primary btn-sm"
+									disabled={ isDisabled }>
+									{ l10n['t_add'] || 'Add' }
+								</button>
+							</div>
 							<span
 								id="release-year.errors"
 								className={ `help-block ${hasValidationErrors ? '' : 'hidden'}` }>
 								{ validationErrors.join(', ') }
 							</span>
-						</div>
-						<div className="form-group form-group-sm">
-							<button type="submit"
-								className="btn btn-primary btn-sm"
-								disabled={ isDisabled }>
-								{ l10n['t_add'] || 'Add' }
-							</button>
-							
-						</div>
-						
-					</form>
+						</form>
+					</div>
 				</div>
-			</div>
+			</React.Fragment>
 		);
 	}
 }
